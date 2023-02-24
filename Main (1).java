ffd65257437de4f45ca74426e1231af4c1da38c2
полина <126289324+czlldmrwn@users.noreply.github.com>
2023-02-24 23:56:59 +0300

    Update Main (1).java

--- Main (1).java
+++ Main (1).java
@@ -7,18 +7,18 @@ public class Main
 	    Scanner scan = new Scanner(System.in);
 		int day = scan.nextInt();
 		switch (day) {
-		    case 1: case 2: case 3: case 4:
-		        System.out.println("Продолжительность рабочего дня "+ 8*60*60 +" секунд");
-		        break;
-	        case 5:
-	            System.out.println("Продолжительность рабочего дня "+ 6*60*60 +" секунд");
-	            break;
-	        case 6: case 7:
-                System.out.println("Выходной день");
-                break;
-            default:
-	            System.out.println("Введен некорректный порядковый номер");
-	            break;
+			case 1: case 2: case 3: case 4:
+				System.out.println("Продолжительность рабочего дня "+ 8*60*60 +" секунд");
+				break;
+			case 5:
+				System.out.println("Продолжительность рабочего дня "+ 6*60*60 +" секунд");
+				break;
+			case 6: case 7:
+				System.out.println("Выходной день");
+				break;
+			default:
+				System.out.println("Введен некорректный порядковый номер");
+				break;
 		}
 	}
 }
